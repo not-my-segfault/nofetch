a9caf4bbd04ec31d113af5cd2e46c7fd21ed95ce
jnats <dev@jnats.xyz>
2021-04-23 23:59:45 +0100

    new install.sh just dropped

--- install.sh
+++ install.sh
@@ -1,4 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
-chmod +x nofetch
-cp nofetch /usr/local/bin/nofetch
+if [ $EUID -eq 0 ]
+then
+        if [ $DEBUG -eq 1 ]
+        then
+                echo "[/] uid 0"
+        fi
+        chmod +x nofetch
+        sudo cp nofetch /usr/local/bin
+elif command -v doas &> /dev/null
+then
+        if [ $DEBUG -eq 1 ]
+        then
+                echo "[/] doas"
+        fi
+        chmod +x nofetch
+        doas cp nofetch /usr/local/bin
+elif command -v sudo &> /dev/null
+then
+        if [ $DEBUG -eq 1 ]
+        then
+                echo "[/] sudo"
+        fi
+        chmod +x nofetch
+        sudo cp nofetch /usr/local/bin
+else
+        echo "\n[X] neither doas nor sudo found, and command isn't running as root, have you checked README.md ?\n"
+fi
