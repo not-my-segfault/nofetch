f0b1e9884776346c1c93d1b429c64cffa5128d48
axtlos <3alouchi2006@gmail.com>
2021-04-24 12:16:40 +0200

    removed redundand and broken stuff

--- install.sh
+++ install.sh
@@ -5,22 +5,19 @@ then
         echo "[/] uid 0"
         rm $(which nofetch > /dev/null 2>&1) > /dev/null 2>&1
         chmod +x nofetch
-        cp nofetch /usr/bin
+        cp nofetch /usr/local/bin
 elif command -v doas &> /dev/null
 then
         echo "[/] doas"
         doas rm $(which nofetch > /dev/null 2>&1) > /dev/null 2>&1
         chmod +x nofetch
-        doas cp nofetch /usr/bin
+        doas cp nofetch /usr/local/bin
 elif command -v sudo &> /dev/null
 then
         echo "[/] sudo"
         sudo rm $(which nofetch > /dev/null 2>&1) > /dev/null 2>&1
         chmod +x nofetch
-        sudo cp nofetch /usr/bin
+        sudo cp nofetch /usr/local/bin
 else
-        echo "[/] neither sudo, nor doas found, using su"
-        su - root "rm $(which nofetch > /dev/null 2>&1) > /dev/null 2>&1"
-        chmod +x nofetch
-        su - root "cp nofetch /usr/bin"
+        echo "[/] neither sudo, nor doas found, run this script as root for it to work"
 fi
