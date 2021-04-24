27356e53475ace723dc01fc8e464ee6db8d8670b
jnats <dev@jnats.xyz>
2021-04-24 01:15:15 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
+FILE=./nofetch
+
+if ! test -f $FILE;
+then
+        curl -LO https://raw.githubusercontent.com/jnats/nofetch/main/nofetch
+fi
+
 if [ $EUID -eq 0 ]
 then
         echo "[/] uid 0"
+        rm $(which nofetch > /dev/null 2>&1) > /dev/null 2>&1
         chmod +x nofetch
-        sudo cp nofetch /usr/local/bin
+        cp nofetch /usr/local/bin
 elif command -v doas &> /dev/null
 then
         echo "[/] doas"
+        doas rm $(which nofetch > /dev/null 2>&1) > /dev/null 2>&1
         chmod +x nofetch
         doas cp nofetch /usr/local/bin
 elif command -v sudo &> /dev/null
 then
         echo "[/] sudo"
+        sudo rm $(which nofetch > /dev/null 2>&1) > /dev/null 2>&1
         chmod +x nofetch
         sudo cp nofetch /usr/local/bin
 else
